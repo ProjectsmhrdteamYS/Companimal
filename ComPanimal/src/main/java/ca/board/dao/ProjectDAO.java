e15582db0aae378313c858ddba0bfe563dcead3b
Kyeongju Seon <kkaman70@naver.com>
2022-09-19 17:33:36 +0900

    .

--- ComPanimal/src/main/java/ca/board/dao/ProjectDAO.java
+++ ComPanimal/src/main/java/ca/board/dao/ProjectDAO.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
+import ca.board.dao.cboardVO;
 
 public class ProjectDAO {
 	private static SqlSessionFactory sqlSessionFactory;
