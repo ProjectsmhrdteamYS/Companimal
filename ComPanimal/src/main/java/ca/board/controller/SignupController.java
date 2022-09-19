d6394db3f448be2015a774ab517fca1dedce8905
taeo <kto1126@gmail.com>
2022-09-19 10:07:25 +0900

    두번째 기초틀

--- ComPanimal/src/main/java/ca/board/controller/SignupController.java
+++ ComPanimal/src/main/java/ca/board/controller/SignupController.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ca.board.dao.userVO;
+
 public class SignupController implements Controller {
 
 	@Override
@@ -14,14 +16,23 @@ public class SignupController implements Controller {
 		// 회원가입 기능
 		String user_id  = request.getParameter("user_id");
 		String user_pw = request.getParameter("user_pw");
-		String name = request.getParameter("user_name");
+		String user_name = request.getParameter("user_name");
 		int user_birth = Integer.parseInt(request.getParameter("user_birth"));
 		int user_tel = Integer.parseInt(request.getParameter("user_tel"));
-		String user_add = request.getParameter("user_add");
+		String user_addr = request.getParameter("user_addr");
 		String user_joindate = request.getParameter("user_joindate");
 		String user_type = request.getParameter("user_type"); // 개인정보 동의
+		userVO vo = new userVO();
+		vo.setUser_id(user_id);
+		vo.setUser_pw(user_pw);
+		vo.setUser_name(user_name);
+		vo.setUser_birth(user_birth);
+		vo.setUser_tel(user_tel);
+		vo.setUser_addr(user_addr);
+		vo.setUser_joindate(user_joindate);
+		vo.setUser_type(user_type);
 		
-		return null;
+		return "redirect:/mainpage.do";
 	}
 
 }
