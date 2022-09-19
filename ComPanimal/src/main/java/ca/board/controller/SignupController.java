2382ce6d2d44ec5ce133528d32128f784755ef59
taeo <kto1126@gmail.com>
2022-09-19 09:46:42 +0900

    기초 틀 잡기

--- ComPanimal/src/main/java/ca/board/controller/SignupController.java
+++ ComPanimal/src/main/java/ca/board/controller/SignupController.java
@@ -12,6 +12,15 @@ public class SignupController implements Controller {
 	public String requestHandler(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// 회원가입 기능
+		String user_id  = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		String name = request.getParameter("user_name");
+		int user_birth = Integer.parseInt(request.getParameter("user_birth"));
+		int user_tel = Integer.parseInt(request.getParameter("user_tel"));
+		String user_add = request.getParameter("user_add");
+		String user_joindate = request.getParameter("user_joindate");
+		String user_type = request.getParameter("user_type"); // 개인정보 동의
+		
 		return null;
 	}
 
